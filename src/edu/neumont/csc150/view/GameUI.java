189903ad3a28bba3fe0b9cbff5d93b0f0ee895f9
Cw Washington <cwashington@student.neumont.edu>
2022-03-08 15:58:33 -0700

    Added BoatMenu

--- src/edu/neumont/csc150/view/GameUI.java
+++ src/edu/neumont/csc150/view/GameUI.java
@@ -7,45 +7,52 @@ import java.io.InputStreamReader;
 public class GameUI {
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-    public void displayMenu(){
-        System.out.println("Prepare for battle: Its BattleShip\r\n"+
+    public void displayMenu() {
+        System.out.println("Prepare for battle: Its BattleShip\r\n" +
                 "Are you ready:\r\n" +
                 "\t1. Player Vs. Player\r\n" +
                 "\t2. Player Vs. the Aliens(Bot)\r\n" +
                 "\t3. Wave your White FLag (that means exit)\r\n");
     }
 
-    public void askGameModeMenu(){
+    public void askGameModeMenu() {
+
         System.out.println("Which game mode will you choose:\r\n" +
                 "\t1. Basic game: (1 shot, next turn)\r\n" +
                 "\t2. Artillery: (Shoot until you miss)\r\n" +
                 "\t3. Salvo: (Your ships = your shots)");
     }
 
-    public void attackMenu(){
+    public void attackMenu() {
+
         System.out.println("It your turn, what shall you do?:\r\n" +
                 "\t1. Shoot your shot\r\n" +
-                "\t2. Wave your white flag\r\n" );
+                "\t2. Wave your white flag\r\n");
+    }
+
+    public void boatMenu(){
+        System.out.println("Where are you stationing your Ships?\r\n");
     }
 
     public int getUserInputAsInt(int min, int max) throws IOException {
         int input;
-        do{
+        do {
             String line = br.readLine();
-            try{
+            try {
                 input = Integer.parseInt(line);
-                if(input > max || input < min){
+                if (input > max || input < min) {
                     throw new NumberFormatException("out of range");
                 }
                 break;
-            } catch(NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println("Input invalid. " +
                         "You must enter a number between " + min + " and " + max);
             }
-        }while (true);
+        } while (true);
         return input;
     }
+
+
 
     public void displayIOError() {
         System.out.println("We ran into an error in IO operations. Let's start over.");
@@ -63,9 +70,9 @@ public class GameUI {
     }
 
     public String askName(boolean isPlayerOne) throws IOException {
-        System.out.print("What is the name of " + (isPlayerOne ? "Player 1":"Player 2") + "? ");
+        System.out.print("What is the name of " + (isPlayerOne ? "Player 1" : "Player 2") + "? ");
         String input = br.readLine();
-        if (input == null || input.trim().isEmpty()){
+        if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException("invalid name");
         }
         return input;
