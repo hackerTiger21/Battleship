39bea53471f4c9b7344dd7e35b91b4da862a8fd0
Brent Ward <b2j2w3@gmail.com>
2022-03-07 19:47:37 -0700

    Board now has takeShot method, returns hit, miss, or already shot

--- src/edu/neumont/csc150/model/Board.java
+++ src/edu/neumont/csc150/model/Board.java
@@ -2,6 +2,7 @@ package edu.neumont.csc150.model;
 
 public class Board {
     private int[][] board = new int[10][10]; //[row][column]
+    //0 - empty water //1 - missed shot //2 - hit shot //3 - undamaged ship
 
     public int[][] getBoard() {
         return board;
@@ -9,5 +10,17 @@ public class Board {
 
     public void setBoard(int[][] board) {
         this.board = board;
+    }
+
+    public int takeShot(int row, int column) { //return 0 if target was chosen already, return 1 for hit, return 2 for miss
+        int target = board[row][column];
+        if (target == 3) {
+            board[row][column] = 2; //you hit a ship
+            return 1;
+        }else if (target == 0) { //you hit empty water
+            board[row][column] = 1;
+            return 2;
+        }
+        return 0;
     }
 }
